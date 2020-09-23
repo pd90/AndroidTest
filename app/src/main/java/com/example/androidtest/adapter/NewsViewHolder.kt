6d8b5de7a025df2dd63fc9c10a0bc3865a1b4062
pdhanta@yahoo.com <pdhanta@yahoo.com>
2020-09-23 18:08:15 +0530

    added the likes and comments and user header changes

--- app/src/main/java/com/example/androidtest/adapter/NewsViewHolder.kt
+++ app/src/main/java/com/example/androidtest/adapter/NewsViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.androidtest.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +10,18 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.androidtest.R
 import com.example.androidtest.data.FeedsModel
+import kotlinx.android.synthetic.main.item_list_header.view.*
 import kotlinx.android.synthetic.main.item_news.view.*
 
 class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun bind(news: FeedsModel?,position: Int?) {
+    @SuppressLint("SetTextI18n")
+    fun bind(news: FeedsModel?, position: Int?) {
         if (news != null) {
-            itemView.txt_news_name.text = news.content
+            itemView.txt_article_content.text = news.content
+            itemView.likes.text = news.likes.toString()
+            itemView.comments.text = news.comments.toString()
+
             if(news.media?.size!! >0) {
                 if (!news.media[0].image.isNullOrEmpty()) {
                     Glide.with(itemView.context)
@@ -24,8 +30,22 @@ class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                             .diskCacheStrategy( DiskCacheStrategy.ALL )
                             .into(itemView.img_article)
                 }
+                itemView.txt_article_title.text = news.media[0].title
+                itemView.txt_article_url.text = news.media[0].url
             }else{
                 itemView.img_article.setImageResource(R.drawable.placeholder)
+            }
+
+            if(news.user.isNotEmpty()) {
+                if (!news.user[0].avatar.isNullOrEmpty()) {
+                    Glide.with(itemView.context)
+                            .load(news.user[0].avatar)
+                            .centerCrop().placeholder(R.drawable.placeholder)
+                            .diskCacheStrategy( DiskCacheStrategy.ALL )
+                            .into(itemView.img_user)
+                }
+                itemView.text_username.text = "${news.user[0].name} ${news.user[0].lastname}"
+                itemView.text_designation.text = news.user[0].designation
             }
         }
     }
